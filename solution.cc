21534671b77c5329cfd22c61b2d0b1a65fcf3c87
DmitryBulin <lostsayron@gmail.com>
2021-12-09 19:45:28 +0300

    Created simple v1.0

---
+++ solution.cc
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+
+std::vector<int> MyOnSort(std::vector<int> arr)
+{
+
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    return arr;
+}
